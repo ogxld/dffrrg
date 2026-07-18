126d816b98ce638bb7756cff12f4c798297e6f1d
ogxld <yebzjs549@gmail.com>
2026-07-18 07:46:47 +0200

    Delete imgbb-config.js

--- imgbb-config.js
+++
@@ -1,3 +0,0 @@
-// Darmowy klucz API z https://api.imgbb.com/ (logowanie tylko emailem, bez karty)
-// Wklej swój klucz poniżej między cudzysłowami.
-const IMGBB_API_KEY = "cee4ef41b942e11137665f0918af26e5";
